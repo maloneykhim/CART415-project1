2e2c9eefbd361911b7042798e45d4944a48a7f87
MollyC98 <chakrabortymollika@gmail.com>
2025-02-18 00:26:05 -0500

    angry anim update molly

--- 415 Game 1/Assets/Scripts/CharaController.cs
+++ 415 Game 1/Assets/Scripts/CharaController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -109,5 +109,126 @@ public class CharaController : MonoBehaviour
             }
             yield return null;
         }
+    }
+}*/
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CharaController : MonoBehaviour
+{
+    public Animator playerAnimator;
+    public Animator rivalAnimator;
+    public Animator explosionAnimator;
+
+    AudioController audioController;
+
+    private float minDuration = 1f;  // Minimum sculpting animation duration
+    private float maxDuration = 5f;  // Maximum sculpting animation duration
+
+    private float nextTriggerTime;
+    private bool isSabotaging = false;
+    private bool isSculpting = false;
+    private bool isCaught = false;
+
+    private void Awake()
+    {
+        audioController = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioController>();
+    }
+
+    void Start()
+    {
+        SetNextTriggerTime();
+        StartCoroutine(CheckCaughtCondition());
+        explosionAnimator.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Prevent actions if the mouse is over a UI element
+        if (EventSystem.current.IsPointerOverGameObject()) return;
+
+        if (Input.GetMouseButton(0))
+        {
+            isSabotaging = true;
+            playerAnimator.SetBool("isSabotaging", true);
+
+            ShowExplosion(); // function to handle explosion visibility
+
+            audioController.PlayPlayerSFX();
+        }
+        else
+        {
+            isSabotaging = false;
+            playerAnimator.SetBool("isSabotaging", false);
+
+            StopExplosion(); // function to fade out explosion
+        }
+
+        if (Time.time >= nextTriggerTime)
+        {
+            Debug.Log("Triggering Sculpting Animation");
+            TriggerAnimation();
+            SetNextTriggerTime();
+        }
+    }
+
+    void TriggerAnimation()
+    {
+        isSculpting = true;
+        rivalAnimator.SetBool("isSculpting", true);
+        audioController.PlayRivalSFX();
+        Invoke(nameof(StopAnimation), Random.Range(minDuration, maxDuration));
+    }
+
+    void StopAnimation()
+    {
+        isSculpting = false;
+        rivalAnimator.SetBool("isSculpting", false);
+        audioController.StopSFX();
+    }
+
+    void SetNextTriggerTime()
+    {
+        nextTriggerTime = Time.time + Random.Range(3f, 7f); //  timing
+    }
+
+    IEnumerator CheckCaughtCondition()
+    {
+        while (true)
+        {
+            if (isSabotaging && isSculpting && !isCaught)
+            {
+                Debug.Log("Caught!");
+                isCaught = true;
+                playerAnimator.SetBool("isCaught", true);
+
+
+                yield return new WaitForSeconds(2f); // Short delay before resetting
+
+                isCaught = false;
+                playerAnimator.SetBool("isCaught", false);
+            }
+            yield return null;
+        }
+    }
+
+    // Handles explosion effect
+    void ShowExplosion()
+    {
+        explosionAnimator.gameObject.SetActive(true);
+        explosionAnimator.SetBool("isExploding", true);
+    }
+
+    void StopExplosion()
+    {
+        explosionAnimator.SetBool("isExploding", false);
+        Invoke(nameof(HideExplosion), 0.5f); // Delay before hiding
+    }
+
+    void HideExplosion()
+    {
+        explosionAnimator.gameObject.SetActive(false);
     }
 }
